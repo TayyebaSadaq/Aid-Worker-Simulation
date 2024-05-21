68b6e6d6e81a16b9431f58f7fb64f03a9653ab34
TayyebaSadaq <official.tayyeba@gmail.com>
2024-05-21 17:47:57 +0100

    tested the settings menu
    make sure project settings -> player -> set to exclusive fullscreen or the toggle needs to be used before it works properly but other than that, successful testing.

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
+
+    public void SetFullscreen(bool isfullscreen)
+    {
+        Screen.fullScreen = isfullscreen;
+    }
+
 }
