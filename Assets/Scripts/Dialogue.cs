05d25a2c4aa4738c787bd545d7b6843acdcb9a7e
TayyebaSadaq <official.tayyeba@gmail.com>
2024-05-22 20:28:25 +0100

    automatic dialogue added

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue(); // Auto-start dialogue when the scene loads
+    }
+
+    // Method to be called by the UI button
+    public void next()
+    {
+        if (textComponent.text == lines[index])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textComponent.text = lines[index];
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        textComponent.text = string.Empty; // Clear the text box before typing the next line
+
+        // Type each character 1 by 1
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false); // Hide the dialogue panel or perform another action when all lines are done
+        }
+    }
+}
